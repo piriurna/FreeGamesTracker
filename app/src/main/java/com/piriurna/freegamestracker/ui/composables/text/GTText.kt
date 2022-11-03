d24bd76b71c8f1c8de7f22395c5a01c9a7767245
FelipeCardonaStudy <felipe.cardona@edu.pucrs.br>
2022-11-02 22:08:15 -0300

    Free Games Tracker theme

---
+++ app/src/main/java/com/piriurna/freegamestracker/ui/composables/text/GTText.kt
@@ -0,0 +1,56 @@
+package com.piriurna.freegamestracker.ui.composables.text
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import com.piriurna.freegamestracker.ui.theme.GTStyle.TextPlay
+import com.piriurna.freegamestracker.ui.theme.GTStyle.TextPlay20
+import com.piriurna.freegamestracker.ui.theme.GTStyle.TextPlayBold
+
+@Composable
+fun GTText(
+    text: String,
+    modifier: Modifier = Modifier,
+    color: Color = MaterialTheme.colors.onPrimary,
+    textAlign: TextAlign? = null,
+    lineHeight: TextUnit = TextUnit.Unspecified,
+    style: TextStyle = TextPlay,
+    letterSpacing: TextUnit = TextUnit.Unspecified,
+    overflow: TextOverflow = TextOverflow.Clip,
+    maxLines: Int = Int.MAX_VALUE,
+) {
+
+    Text(
+        text = text,
+        modifier = modifier,
+        color = color,
+        textAlign = textAlign,
+        lineHeight = lineHeight,
+        style = style,
+        letterSpacing = letterSpacing,
+        overflow = overflow,
+        maxLines = maxLines
+    )
+}
+
+@Preview(showBackground = false)
+@Composable
+private fun FGTextPreview() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        GTText(text = "Teste 1")
+        GTText(text = "Teste 2", style= TextPlayBold)
+        GTText(text = "Teste 3", style = TextPlay20)
+    }
+}
--- app/src/main/java/com/piriurna/freegamestracker/ui/theme/Color.kt
+++ app/src/main/java/com/piriurna/freegamestracker/ui/theme/Color.kt
@@ -2,7 +2,15 @@ package com.piriurna.freegamestracker.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+
+val Salmon = Color(0xFFFC7786)
+val Blue = Color(0xFF6A67F3)
+val Green = Color(0xFF5CF64A)
+val Golden = Color(0xFFD98324)
+
+val Violet = Color(0xFF1E183D)
+val Grape = Color(0xFF463F71)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFBFBFB)
+val Gray = Color(0xFFCCCCCC)
+val Red = Color(0xFFB80C09)
