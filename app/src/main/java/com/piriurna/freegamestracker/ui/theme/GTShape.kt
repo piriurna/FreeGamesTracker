06b77b3dca49d2e824930b62be7fe7cc59ea2dec
FelipeCardonaStudy <felipe.cardona@edu.pucrs.br>
2022-11-02 22:08:15 -0300

    Free Games Tracker theme

---
+++ app/src/main/java/com/piriurna/freegamestracker/ui/theme/GTShape.kt
@@ -0,0 +1,13 @@
+package com.piriurna.freegamestracker.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Shapes
+import androidx.compose.ui.unit.dp
+
+object GTShape {
+    val Shapes = Shapes(
+        small = RoundedCornerShape(16.dp),
+        medium = RoundedCornerShape(20.dp),
+        large = RoundedCornerShape(24.dp)
+    )
+}
---
+++ app/src/main/java/com/piriurna/freegamestracker/ui/theme/GTTheme.kt
@@ -0,0 +1,49 @@
+package com.piriurna.freegamestracker.ui.theme
+
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
+import androidx.compose.runtime.Composable
+
+private val DarkColorPalette = darkColors(
+    primary = Salmon,
+    secondary = Blue,
+    background = Violet,
+    surface = Grape,
+    onPrimary = White,
+    onSecondary = White,
+    onBackground = White,
+    onSurface = White,
+    error = Red,
+    onError = White
+)
+
+private val LightColorPalette = lightColors(
+    primary = Salmon,
+    secondary = Blue,
+    background = Violet,
+    surface = Grape,
+    onPrimary = White,
+    onSecondary = White,
+    onBackground = White,
+    onSurface = White,
+    error = Red,
+    onError = White
+)
+
+@Composable
+fun FreeGamesTrackerTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+    val colors = if (darkTheme) {
+        DarkColorPalette
+    } else {
+        LightColorPalette
+    }
+
+    MaterialTheme(
+        colors = colors,
+        typography = GTType.Typography,
+        shapes = GTShape.Shapes,
+        content = content
+    )
+}
