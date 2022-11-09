2b49f2f6b769c2779546ac866a1ae9236590aeb0
Franco <francozalamena@gmail.com>
2022-11-09 17:07:59 +0000

    GTLoading

---
+++ app/src/main/java/com/piriurna/freegamestracker/ui/composables/animations/GTLottieLoading.kt
@@ -0,0 +1,57 @@
+package com.piriurna.freegamestracker.ui.composables.animations
+
+import androidx.annotation.RawRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.airbnb.lottie.compose.*
+import com.piriurna.freegamestracker.R
+
+@Composable
+fun GTLottieLoading(
+    modifier: Modifier = Modifier,
+    @RawRes lottieId: Int = R.raw.loading,
+) {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(lottieId)
+    )
+
+    val isPlaying by remember {
+        mutableStateOf(true)
+    }
+
+    val speed by remember {
+        mutableStateOf(1f)
+    }
+
+    val progress by animateLottieCompositionAsState(
+        composition,
+        iterations = LottieConstants.IterateForever,
+        isPlaying = isPlaying,
+        speed = speed,
+        restartOnPlay = false
+    )
+
+    LottieAnimation(
+        composition = composition,
+        progress = { progress },
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GTLottieLoadingPreview() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        GTLottieLoading(Modifier.size(400.dp))
+    }
+
+}
